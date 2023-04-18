cd4140d0c239cb9b19d2128b69fe7ba4ae58f0da
Iris Membrere <irismembrere@gmail.com>
2023-04-18 21:52:14 +0800

    Getting an input from the user

---
+++ Assignment 2.1_Encryption.py
@@ -0,0 +1,9 @@
+# Zemerelin Iris M. Membrere
+# BSCPE 1-4
+# Assignment 2
+
+# Problem 1 - Encryption
+
+# Get an input from the user
+ask_input = input("\033[0;31;48mInput a plaintext: \033[35m")
+text_output = ("")
