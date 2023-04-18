3c6a723422702d0cb6dfa11994f68325b06fa0b9
Iris Membrere <irismembrere@gmail.com>
2023-04-18 21:58:52 +0800

    Check if the input has the variables and change it into corresponding symbols

--- Assignment 2.1_Encryption.py
+++ Assignment 2.1_Encryption.py
@@ -7,3 +7,6 @@
 # Get an input from the user
 ask_input = input("\033[0;31;48mInput a plaintext: \033[35m")
 text_output = ("")
+
+# Check if the input has the variables and change it into corresponding symbols
+for i in range(len(ask_input)):
